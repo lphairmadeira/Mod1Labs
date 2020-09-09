fe3b9c57073c4e57377189deab4c09ca26fa7172
Lindsay Phair <lindsayphair@lindsays-air.myfiosgateway.com>
2020-09-09 11:16:26 -0400

    Add existing file

--- src/madeira/cs/Main.java
+++ src/madeira/cs/Main.java
@@ -3,6 +3,44 @@ package madeira.cs;
 public class Main {
 
     public static void main(String[] args) {
-	//
+        // Here is my first string!
+        String myString1 = "This is my first string";
+        System.out.println(myString1);
+        // Here is my second string
+        String myString2 = "This is my second string I hope it works";
+        System.out.println(myString2);
+        //Finally, here is my third string
+        String myString3 = "Final string PLEASE WORK";
+        System.out.println(myString3);
+        //Okay it worked!! Next I have to reassign them.
+        myString1 = "I am changing my first string";
+        myString2 = "I am changing my second string";
+        myString3 = "I am changing my third string";
+        System.out.println(myString1);
+        System.out.println(myString2);
+        System.out.println(myString3);
+        //Success! Now on to the methods.
+        myString1.charAt(13);
+        myString2.compareTo(myString1);
+        myString3.equals(myString2);
+        //I have no idea what any of those commands did but I finished with exit code 0!
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+
 }
