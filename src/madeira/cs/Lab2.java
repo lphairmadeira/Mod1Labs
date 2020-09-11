0bb92b3ebaf352c5d32fe04a8467d229ac6ab54d
Lindsay Phair <lindsayphair@lindsays-air.myfiosgateway.com>
2020-09-10 22:35:42 -0400

    Final changes

--- src/madeira/cs/Lab2.java
+++ src/madeira/cs/Lab2.java
@@ -10,17 +10,17 @@ public class Lab2 {
     String greeting = "Hi " + name;
     System.out.println(greeting);
     //Part 2
-    System.out.println("Enter your favorite artist");
+    System.out.println("Enter my favorite artist to reveal my favorite song");
+    //You must type in "Harry Styles" because he is my favorite artist!
     String artist = s.nextLine();
-    System.out.println("Your favorite Harry Styles song is Canyon Moon");
+    System.out.println("My favorite Harry Styles song is Canyon Moon");
     //Part 3
     System.out.println("Enter your age");
     Integer age = s.nextInt();
     System.out.println(age * 365);
     String myString8 = "15";
     new Scanner(myString8);
-    String myString9 = "5,615";
-    System.out.println(myString9);
+    //All Done!
 
 }
 }
