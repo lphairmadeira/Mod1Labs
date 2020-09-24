66b109ebd3f47ce8b44eeabdf7b5c90c2b87b491
Lindsay Phair <lindsayphair@lindsays-air.myfiosgateway.com>
2020-09-24 11:51:51 -0400

    Working on superlab

--- src/madeira/cs/SuperLab.java
+++ src/madeira/cs/SuperLab.java
@@ -3,68 +3,77 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class SuperLab {
-    public static String main(String[] args) {
+    public static void main(String[] args) {
         try {
             File myObj = new File("/Users/lindsayphair/Downloads/artistList.txt");
             Scanner myReader = new Scanner(myObj);
-
+            int numline = myReader.nextInt();
+            String temp = myReader.nextLine();
+            String [] artists = new String [numline];
+            int counter = 0;
             while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
+                artists[counter] = myReader.nextLine();
+                counter ++;
             }
             myReader.close();
+
+            Artist [] artists1 = new Artist [numline];
+            counter = 0;
+            for (String line: artists) {
+                artists1 [counter].setName (line.split(",")[0]);
+                artists1 [counter].setAlbum (line.split(",")[0]);
+                /*
+                line.split(",")[0];
+                line.split(",")[1];
+                line.split(",")[2];
+                 */
+                counter++;
+            }
+
+
+
         }
 
         catch (FileNotFoundException e) {
             System.out.println("An error occurred");
-            e.printStackTrace(); }
-
-        public class Artist {
-            public Artist(String name, String album, String TwitterHandle) {
-            }
-            Artist BillieEilish = new Artist("Billie Eilish", "Don't smile at me", "@BillieEilish");
-           Artist EARTHGANG = new Artist("EARTHGANG", "Shallow Graves for Toys", "@EarthGang");
-           Artist GlassAnimals = new Artist ("Glass Animals", "Dreamland", "@GlassAnimals");
-           Artist ArticMonkeys = new Artist ("The Arctic Monkeys", "AM", "@ArcticMonkeys");
-           Artist TheStrokes = new Artist ("The Strokes", "The New Abnormal", "@thestrokes");
+            e.printStackTrace();
         }
+/*
+"Billie Eilish", "Don't smile at me", "@BillieEilish"
+"EARTHGANG", "Shallow Graves for Toys", "@EarthGang"
+"Glass Animals", "Dreamland", "@GlassAnimals"
+"The Arctic Monkeys", "AM", "@ArcticMonkeys"
+"The Strokes", "The New Abnormal", "@thestrokes"
+ */
+
+    }
+}
+        class Artist {
             String name;
             String album;
             String twitterHandle;
+            int rating;
 
-    public String BillieEilish(); {
-    return name + "released her album" + album + "\n Her twitter handle is" + twitterHandle; }
+            public Artist() {
+                name = "n/a";
+                album = "n/a";
+                twitterHandle = "n/a";
+                rating = 0;
+            }
+            Scanner s = new
+                    Scanner(System.in);
+            System.out.println("What do you think of" + name + "Enter a rating 1-10");
+            int rating = s.nextInt();
 
-    public Artist EARTHGANG(); {
-    return name + "released their album" + album + "\n Their twitter handle is" + twitterHandle; }
-
-    public Artist GlassAnimals(); {
-    return name + "released their album" + album + "\n Their twitter handle is" + twitterHandle; }
-
-    public Artist ArcticMonkeys(); {
-    return name + "released their album" + album + "\n Their twitter handle is" + twitterHandle; }
-
-    public Artist TheStrokes(); {
-    return name + "released their album" + album + "\n Their twitter handle is" + twitterHandle;
-}
-
-
+            public String getName() { return name; }
+            public String getAlbum() { return album; }
+            public String getTwitter() {return twitterHandle; }
+            public int getRating() {return rating; }
+            public void setName(String newName) { name = newName; }
+            public void setAlbum(String newAlbum) {album = newAlbum;}
+            public void setTwitter(String newTwitter) {twitterHandle = newTwitter;}
+            public void setRating(int newRating) {rating = newRating;}
 
 
 
         }
-        }
-
-
-/* Exercise I----------In Type I syntax (i.e. String [] list = {"item1", "item2"}
-construct an array of the planets which are:
-Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune and PlutoYes, Pluto too, he's cute.
-Print out these planets with the cool new foreach loopa la: for (String listItem: myList )
-
-        String [] list = {"Yes"};
-        for (String listItem: myList){
-            System.out.println(myList);
-
-        */
-
-    }
